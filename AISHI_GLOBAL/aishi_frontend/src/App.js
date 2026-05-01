cb1ba08608e103191b79e9413b33d9ca261154e8
Karthik K <karthik1842001@gmail.com>
2026-05-01 17:48:04 +0530

    update website

--- AISHI_GLOBAL/aishi_frontend/src/App.js
+++ AISHI_GLOBAL/aishi_frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import AboutSection from './components/AboutSection';
 import Hero from './components/Hero';
 import Gallery from './components/Gallery';
@@ -29,7 +29,7 @@ function Home() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
 
         {/* Homepage */}
@@ -39,7 +39,7 @@ function App() {
         <Route path="/service/:name" element={<ServicePage />} />
 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
